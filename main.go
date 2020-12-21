133e1efb2ebe4fa129a8084a8fe0b124c27ebf6f
isekai <brian1689878638@gmail.com>
2020-12-22 00:45:56 +0800

    Add a log function to DataStreams

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"iot-backend/pkg/chtapi"
-	"log"
 
 	_ "github.com/joho/godotenv/autoload"
 )
@@ -11,17 +10,5 @@ func main() {
 	client := TestClient()
 
 	dataStream := chtapi.SubAllSensors(client, 100)
-
-	logChan := make(chan string)
-	for _, v := range dataStream {
-		go func(payloadChan chan []byte) {
-			for payload := range payloadChan {
-				logChan <- string(payload)
-			}
-		}(v)
-	}
-
-	for logMsg := range logChan {
-		log.Println(logMsg)
-	}
+	dataStream.LogAll()
 }
--- pkg/chtapi/mqtt_utils.go
+++ pkg/chtapi/mqtt_utils.go
@@ -7,13 +7,12 @@ import (
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
-// type struct DataStream {
-
-// }
+// DataStreams stores channels listened for a topic from mqtt
+type DataStreams map[SensorPair]chan []byte
 
 // SubAllSensors subscribe all sensors and return a map of channels that streaming paylods by mqtt.
-func SubAllSensors(client mqtt.Client, bufsize int) map[SensorPair]chan []byte {
-	dataStream := make(map[SensorPair]chan []byte)
+func SubAllSensors(client mqtt.Client, bufsize int) DataStreams {
+	dataStream := make(DataStreams)
 	for v := range ListAllSensors() {
 		topic := fmt.Sprintf("/v1/device/%s/sensor/%s/rawdata", v.Device.ID, v.Sensor.ID)
 		log.Println(topic)
@@ -27,4 +26,21 @@ func SubAllSensors(client mqtt.Client, bufsize int) map[SensorPair]chan []byte {
 		}(dataStream[v])
 	}
 	return dataStream
+}
+
+// LogAll logs all data from streams
+func (dataStreams DataStreams) LogAll() {
+	logChan := make(chan string)
+
+	for _, v := range dataStreams {
+		go func(payloadChan chan []byte) {
+			for payload := range payloadChan {
+				logChan <- string(payload)
+			}
+		}(v)
+	}
+
+	for logMsg := range logChan {
+		log.Println(logMsg)
+	}
 }
