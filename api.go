985da16a8b268dae38b0b4a88d07db3c4acf8473
isekai <brian1689878638@gmail.com>
2021-01-02 23:48:56 +0800

    Test CORS

--- api.go
+++ api.go
@@ -8,6 +8,9 @@ import (
 func CORSAllowAll() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Header("Access-Control-Allow-Origin", "http://localhost:8080")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 		c.Next()
 	}
 }
